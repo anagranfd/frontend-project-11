f892ff5172bb86d9c4cd035edd72a0e8bbb925c1
anagran <edumfed@gmail.com>
2023-07-31 12:12:38 +0400

    step 2 - form validation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 // Import our custom CSS
 import './scss/styles.scss';
-
-// Import all of Bootstrap's JS
-// import * as bootstrap from 'bootstrap';
 import 'bootstrap';
 
-console.log('Hello World!');
+import app from './app';
+
+app();
